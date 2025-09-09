2be2379a5d6fc8d8ea331d0ac1cad99b3fbd09ec
safaryan elen <safaryan.elen.00@list.ru>
2025-09-09 20:19:20 +0300

    Update Green.cs

--- Lab1/Green.cs
+++ Lab1/Green.cs
@@ -1,4 +1,5 @@
-﻿namespace Lab1
+using System;
+namespace Lab1
 {
     public class Green
     {
@@ -6,70 +7,96 @@
         {
             bool answer = false;
 
-            // code here
-
-            // end
+            if (Math.Abs(d) >= 1)
+            {
+                answer = true;
+            }
 
             return answer;
+
+            //done
         }
         public bool Task2(double d, double f)
         {
             bool answer = false;
 
-            // code here
+            double avg = (d + f) / 2;
 
-            // end
+            if (avg > 0)
+            {
+                answer = true;
+            }
 
             return answer;
+            //done
         }
         public bool Task3(int a, int b)
         {
             bool answer = false;
 
-            // code here
-
-            // end
+            int sum = a + b;
+            int na = Math.Abs(a);
+            int nb = Math.Abs(b);
+            double avg = (na + nb) / 2;
+            if (sum > avg)
+            {
+                answer = true;
+            }
 
             return answer;
+            //done
         }
         public int Task4(int a, int b, int c)
         {
             int answer = 0;
 
-            // code here
-
-            // end
+            int maxi = Math.Max(a, b);
+            int maxj = Math.Max(c, maxi);
+            answer = maxj;
 
             return answer;
+            //done
         }
         public double Task5(double x)
         {
             double answer = 0;
 
-            // code here
-
-            // end
+            if (Math.Abs(x) > 1) {
+                answer = 0;
+            }
+            else if (Math.Abs(x) <= 1)
+            {
+                answer = Math.Pow(x, 2) - 1;
+            }
 
             return answer;
+            //done
         }
         public bool Task6(double x, double y)
         {
             bool answer = false;
 
-            // code here
+            if (y < 0) answer = false;
+            else if (x < -1 || x > 1) answer = false;
+            else if (x < 0) return answer = y <= 1 + x;
+            else if (x >= 0) return answer = y <= 1 - x;
 
-            // end
-
-            return answer;
+                return answer;
+            //done
         }
 
         public bool Task7(int n)
         {
             bool answer = true;
 
-            // code here
-
-            // end
+            if (n < 0)
+            {
+                answer = false;
+            }
+            else if ( n % 2 == 0)
+            {
+                answer = false;
+            }
 
             return answer;
         }
@@ -77,9 +104,10 @@
         {
             bool answer = false;
 
-            // code here
-
-            // end
+            if ((X * 60 >= 420) && (360 >= (Y * (X + 1) / 2)) && (Y * (X + 1) / 2) >= 240)
+            {
+                answer = true;
+            }
 
             return answer;
         }
